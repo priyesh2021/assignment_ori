af83921210b316c1aea9d68765c3fd1a1286438b
priyesh2021 <priyesh1167.be20@chitkarauniversity.edu.in>
2024-09-05 23:53:05 +0000

    v1.4

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-echo "This is my initial commit";
-echo "This is my second commit";
-echo "This is my third commit";
-echo "This is my fourth commit";
+echo "This is my initial commit \n second commit";
 
 echo `ifconfig`;
 
